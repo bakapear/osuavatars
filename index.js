808ede7393c3ebaf0a6ac99bd3c0d4861c98b697
bakapear <proxybomb@gmail.com>
2019-02-26 13:45:55 +0100

    updated style

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-let got = require("got")
-let cheerio = require("cheerio")
-let fs = require("fs")
+let got = require('got')
+let cheerio = require('cheerio')
+let fs = require('fs')
 
-let url = "https://www.osustuff.org/img/avatars/base-images/"
+let url = 'https://www.osustuff.org/img/avatars/base-images/'
 got(url).then(async r => {
-    let $ = cheerio.load(r.body)
-    let tbody = $("tbody")[0]
-    let links = []
-    for (let i = 5; i < tbody.children.length; i++) {
+  let $ = cheerio.load(r.body)
+  let tbody = $('tbody')[0]
+  let links = []
+  for (let i = 5; i < tbody.children.length; i++) {
+    if (tbody.children[i] && tbody.children[i].children && tbody.children[i].children[1]) {
+      links.push(tbody.children[i].children[1].children[0].attribs.href)
+    }
+  }
+  let promises = []
+  for (let i = 0; i < links.length; i++) {
+    let base = 'https://www.osustuff.org/img/avatars/base-images/'
+    promises.push(got(base + links[i]))
+  }
+  await Promise.all(promises).then(p => {
+    let arr = p.map(x => {
+      let $ = cheerio.load(x.body)
+      let tbody = $('tbody')[0]
+      let links = []
+      let pre = $('h1').text().match(/\/.*/)[0]
+      for (let i = 5; i < tbody.children.length; i++) {
         if (tbody.children[i] && tbody.children[i].children && tbody.children[i].children[1]) {
-            links.push(tbody.children[i].children[1].children[0].attribs.href)
+          if (tbody.children[i].children[1].children[0].attribs.href.indexOf('thumb') >= 0) continue
+          links.push(pre + '/' + tbody.children[i].children[1].children[0].attribs.href)
         }
-    }
-    let promises = []
-    for (let i = 0; i < links.length; i++) {
-        let base = "https://www.osustuff.org/img/avatars/base-images/"
-        promises.push(got(base + links[i]))
-    }
-    let all = []
-    await Promise.all(promises).then(p => {
-        let arr = p.map(x => {
-            let $ = cheerio.load(x.body)
-            let tbody = $("tbody")[0]
-            let links = []
-            let pre = $("h1").text().match(/\/.*/)[0]
-            for (let i = 5; i < tbody.children.length; i++) {
-                if (tbody.children[i] && tbody.children[i].children && tbody.children[i].children[1]) {
-                    if (tbody.children[i].children[1].children[0].attribs.href.indexOf("thumb") >= 0) continue
-                    links.push(pre + "/" + tbody.children[i].children[1].children[0].attribs.href)
-                }
-            }
-            return links
-        })
-        arr = [].concat.apply([], arr)
-        fs.writeFileSync("data.json", JSON.stringify({ date: Date.now(), url: "https://www.osustuff.org", items: arr }, null, 4))
+      }
+      return links
     })
+    arr = [].concat.apply([], arr)
+    fs.writeFileSync('data.json', JSON.stringify({ date: Date.now(), url: 'https://www.osustuff.org', items: arr }, null, 4))
+  })
 })
