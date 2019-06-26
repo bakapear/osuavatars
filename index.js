9cf69cf750cd7f5b5f00dc94398e89cb3d010b9e
bakapear <proxybomb@gmail.com>
2019-06-26 12:42:41 +0200

    rewrite

--- index.js
+++ index.js
@@ -3,35 +3,23 @@ let cheerio = require('cheerio')
 let fs = require('fs')
 
 let url = 'https://www.osustuff.org/img/avatars/base-images/'
-got(url).then(async r => {
-  let $ = cheerio.load(r.body)
-  let tbody = $('tbody')[0]
-  let links = []
-  for (let i = 5; i < tbody.children.length; i++) {
-    if (tbody.children[i] && tbody.children[i].children && tbody.children[i].children[1]) {
-      links.push(tbody.children[i].children[1].children[0].attribs.href)
-    }
-  }
-  let promises = []
-  for (let i = 0; i < links.length; i++) {
-    let base = 'https://www.osustuff.org/img/avatars/base-images/'
-    promises.push(got(base + links[i]))
-  }
-  await Promise.all(promises).then(p => {
-    let arr = p.map(x => {
-      let $ = cheerio.load(x.body)
-      let tbody = $('tbody')[0]
-      let links = []
-      let pre = $('h1').text().match(/\/.*/)[0]
-      for (let i = 5; i < tbody.children.length; i++) {
-        if (tbody.children[i] && tbody.children[i].children && tbody.children[i].children[1]) {
-          if (tbody.children[i].children[1].children[0].attribs.href.indexOf('thumb') >= 0) continue
-          links.push(pre + '/' + tbody.children[i].children[1].children[0].attribs.href)
-        }
-      }
-      return links
-    })
-    arr = [].concat.apply([], arr)
-    fs.writeFileSync('data.json', JSON.stringify({ date: Date.now(), url: 'https://www.osustuff.org', items: arr }, null, 4))
-  })
-})
+
+async function main () {
+  let { body } = await got(url)
+  let $ = cheerio.load(body)
+  let links = Array.from($('tr > td > a')).slice(1).map(x => x.attribs.href)
+  let res = await getImages(links)
+  fs.writeFileSync('data.json', JSON.stringify({ date: Date.now(), url: 'https://www.osustuff.org/img/avatars/base-images/', items: res }, null, 4))
+}
+
+async function getImages (arr, res = [], i = 0) {
+  if (i >= arr.length) return
+  console.log(`${arr[i]} - ${i + 1}/${arr.length} [${(((i + 1) / arr.length) * 100).toFixed(2)}%]`)
+  let { body } = await got(url + arr[i])
+  let $ = cheerio.load(body)
+  res.push(Array.from($('tr > td > a')).slice(1).map(x => arr[i] + x.attribs.href).filter(x => x.indexOf('_thumb.') === -1))
+  await getImages(arr, res, ++i)
+  return [].concat.apply([], res)
+}
+
+main()
